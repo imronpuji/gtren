28390caee4fc1041deaaf618c0174c97978389f9
imronpuji <imronpuji5@gmail.com>
2021-06-09 15:41:21 +0700

    'initial-package'

--- app/Views/auth/register.php
+++ app/Views/auth/register.php
@@ -7,8 +7,14 @@
     <?php endif; ?>
     <div class="mb-3">
         <label for="exampleInputEmail1" class="form-label">Nama</label>
-        <input type="text" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" name="nama">
-        <div id="emailHelp" class="form-text">We'll never share your email with anyone else.</div>
+        <input type="text" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" name="nama"><br>
+         <label for="exampleInputEmail1" class="form-label">Phone</label>
+        <input type="text" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" name="phone"><br>
+         <label for="exampleInputEmail1" class="form-label">Email</label>
+        <input type="text" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" name="email"><br>
+         <label for="exampleInputEmail1" class="form-label">Alamat</label>
+        <input type="text" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" name="alamat">
+
     </div>
     <button type="submit" class="btn btn-primary" name="button">Submit</button>
 </form>
